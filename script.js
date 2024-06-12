2760616f1f0a3a060371c371b68d06456fa25a7d
Wafflegum <70809819+Wafflegum@users.noreply.github.com>
2024-06-12 17:51:02 +0800

    desktop version is done!

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const currentWork = document.getElementById('current-work');
 const previousWork = document.getElementById('prev-work');
 
+const currentPlay = document.getElementById('current-play');
+const previousPlay = document.getElementById('prev-play');
+
+const currentStudy = document.getElementById('current-study');
+const previousStudy = document.getElementById('prev-study');
+
+const currentExercise = document.getElementById('current-exercise');
+const previousExercise = document.getElementById('prev-exercise');
+
+const currentSocial = document.getElementById('current-social');
+const previousSocial = document.getElementById('prev-social');
+
+const currentSelfCare = document.getElementById('current-selfcare');
+const previousSelfCare = document.getElementById('prev-selfcare');
+
 var infoData
-console.log(previousWork)
 
 function assignVar(data) {
     infoData = data
@@ -17,8 +31,31 @@ fetch("data.json")
 function assignStats() {
     if(infoData !== null || infoData.Length !== 0) {
         var work = infoData.find(data => data.title === "Work");
+        var play = infoData.find(data => data.title === "Play");
+        var study = infoData.find(data => data.title === "Study");
+        var exercise = infoData.find(data => data.title === "Exercise");
+        var social = infoData.find(data => data.title === "Social");
+        var selfcare = infoData.find(data => data.title === "Self Care");
+
         currentWork.textContent = `${work.timeframes.weekly.current}hrs`
         previousWork.textContent = `Last week - ${work.timeframes.weekly.previous}hrs`
+
+        currentPlay.textContent = `${play.timeframes.weekly.current}hrs`
+        previousPlay.textContent = `Last week - ${play.timeframes.weekly.previous}hrs`
+
+        currentStudy.textContent = `${study.timeframes.weekly.current}hrs`
+        previousStudy.textContent = `Last week - ${study.timeframes.weekly.previous}hrs`
+
+        currentExercise.textContent = `${exercise.timeframes.weekly.current}hrs`
+        previousExercise.textContent = `Last week - ${exercise.timeframes.weekly.previous}hrs`
+
+        currentSocial.textContent = `${social.timeframes.weekly.current}hrs`
+        previousSocial.textContent = `Last week - ${social.timeframes.weekly.previous}hrs`
+
+        currentSelfCare.textContent = `${selfcare.timeframes.weekly.current}hrs`
+        previousSelfCare.textContent = `Last week - ${selfcare.timeframes.weekly.previous}hrs`
+
+
     }
 }
 

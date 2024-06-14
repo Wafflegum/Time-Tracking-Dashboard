ba2ca977eb7260845eb7928df57816774a0b80ac
Wafflegum <70809819+Wafflegum@users.noreply.github.com>
2024-06-14 16:18:13 +0800

    Project done!
    Mobile version is done. Timeframe buttons are functional as well.

---
+++ README.md
@@ -0,0 +1,54 @@
+# Frontend Mentor - Time tracking dashboard solution
+
+This is a solution to the [Time tracking dashboard challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+A basic timetracking website.
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Switch between viewing Daily, Weekly, and Monthly stats
+
+### Screenshot
+
+![Desktop version](./preview/Desktop%20version.png)
+![Mobile version](./preview/Mobile%20version.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://www.frontendmentor.io/solutions/responsive-time-tracking-dashboard-using-css-flex-css-grid-t4TmArNQMP)
+- Live Site URL: [Add live site URL here](https://wafflegum.github.io/Time-Tracking-Dashboard/)
+
+### Built with
+- HTML
+- Flexbox
+- CSS Grid
+- Javascript
+- [Sass](https://sass-lang.com/) - JS library
+
+### What I learned
+
+I'm really proud of learning grid! Before this, I found grid useless as I really like how flex works. Now I've learned that flex is more of a responsive display-type and grid will be more of a fixed or structured display-type.
+
+I've learned for of and Object.entries, which I think will help me in the future! Although I don't think I can implement it on the spot, it's good that I have been introduced with it by ChatGPT. I've also learned how to fetch JSON data from external files, which was quite a headache to learn as it's really a different approach.
+
+## Author
+
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/Wafflegum)
--- script.js
+++ script.js
@@ -1,61 +1,77 @@
-const currentWork = document.getElementById('current-work');
-const previousWork = document.getElementById('prev-work');
+const elements = {
+    "Work": {
+        current: document.getElementById('current-work'),
+        previous: document.getElementById('prev-work')
+    },
+    "Play": {
+        current: document.getElementById('current-play'),
+        previous: document.getElementById('prev-play')
+    },
+    "Study": {
+        current: document.getElementById('current-study'),
+        previous: document.getElementById('prev-study')
+    },
+    "Exercise": {
+        current: document.getElementById('current-exercise'),
+        previous: document.getElementById('prev-exercise')
+    },
+    "Social": {
+        current: document.getElementById('current-social'),
+        previous: document.getElementById('prev-social')
+    },
+    "Self Care": {
+        current: document.getElementById('current-selfcare'),
+        previous: document.getElementById('prev-selfcare')
+    }
+} 
 
-const currentPlay = document.getElementById('current-play');
-const previousPlay = document.getElementById('prev-play');
+const dailyBtn = document.getElementById('daily')
+const weeklyBtn = document.getElementById('weekly')
+const monthlyBtn = document.getElementById('monthly')
 
-const currentStudy = document.getElementById('current-study');
-const previousStudy = document.getElementById('prev-study');
+let openedTimeframe
 
-const currentExercise = document.getElementById('current-exercise');
-const previousExercise = document.getElementById('prev-exercise');
-
-const currentSocial = document.getElementById('current-social');
-const previousSocial = document.getElementById('prev-social');
-
-const currentSelfCare = document.getElementById('current-selfcare');
-const previousSelfCare = document.getElementById('prev-selfcare');
-
-var infoData
+let infoData
 
 function assignVar(data) {
     infoData = data
-    console.log(infoData)
-    assignStats()
+    assignStats('daily')
+    toggleButton(dailyBtn)
 }
 
 fetch("data.json")
     .then(response => response.json())
     .then(data => assignVar(data))
 
-function assignStats() {
+function assignStats(timeframe) {
     if(infoData !== null || infoData.Length !== 0) {
-        var work = infoData.find(data => data.title === "Work");
-        var play = infoData.find(data => data.title === "Play");
-        var study = infoData.find(data => data.title === "Study");
-        var exercise = infoData.find(data => data.title === "Exercise");
-        var social = infoData.find(data => data.title === "Social");
-        var selfcare = infoData.find(data => data.title === "Self Care");
-
-        currentWork.textContent = `${work.timeframes.weekly.current}hrs`
-        previousWork.textContent = `Last week - ${work.timeframes.weekly.previous}hrs`
-
-        currentPlay.textContent = `${play.timeframes.weekly.current}hrs`
-        previousPlay.textContent = `Last week - ${play.timeframes.weekly.previous}hrs`
-
-        currentStudy.textContent = `${study.timeframes.weekly.current}hrs`
-        previousStudy.textContent = `Last week - ${study.timeframes.weekly.previous}hrs`
-
-        currentExercise.textContent = `${exercise.timeframes.weekly.current}hrs`
-        previousExercise.textContent = `Last week - ${exercise.timeframes.weekly.previous}hrs`
-
-        currentSocial.textContent = `${social.timeframes.weekly.current}hrs`
-        previousSocial.textContent = `Last week - ${social.timeframes.weekly.previous}hrs`
-
-        currentSelfCare.textContent = `${selfcare.timeframes.weekly.current}hrs`
-        previousSelfCare.textContent = `Last week - ${selfcare.timeframes.weekly.previous}hrs`
-
-
+        for (const [title, element] of Object.entries(elements)) { //* sets the element values of each cards in the screen by using one by one based on the timeframe inputted
+            let activityData = infoData.find(data => data.title === title)
+            element.current.textContent = `${activityData.timeframes[timeframe].current}hrs`
+            element.previous.textContent = `Last week - ${activityData.timeframes[timeframe].previous}hrs`
+        }
     }
 }
 
+dailyBtn.addEventListener('click', () => {
+    assignStats('daily')
+    toggleButton(dailyBtn)
+})
+weeklyBtn.addEventListener('click', () => {
+    assignStats('weekly')
+    toggleButton(weeklyBtn)
+})
+monthlyBtn.addEventListener('click', () => {
+    assignStats('monthly')
+    toggleButton(monthlyBtn)
+})
+
+function toggleButton(element) {
+    var opened = document?.getElementById(openedTimeframe)
+    if(opened)
+    opened.style.color = 'hsl(235, 45%, 61%)'
+
+    element.style.color = 'white'
+    openedTimeframe = element.id
+
+}
